650ceeb3b06b108401861350626d07ce3e5476ae
Gabriel Dantas <48440319+gbrfilipe@users.noreply.github.com>
2025-07-20 22:34:32 +0100

    fix dag import

--- weather_etl/dag.py
+++ weather_etl/dag.py
@@ -1,7 +1,33 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from pendulum import datetime
-from .utils import load_cities, dict_cities
+import requests as req
+
+def load_cities() -> list:
+    """
+    Call an URL to load a JSON file with city data.
+    """
+
+    url = "https://www.jsonkeeper.com/b/L3YT"
+
+    try:
+        response = req.get(url, timeout=10)
+        response.raise_for_status()
+        return response.json()
+    except req.RequestException as e:
+        print(f"Error fetching cities: {e}")
+        return []
+
+def dict_cities(ti) -> dict:
+    """
+    Convert a list of city dictionaries into a dictionary indexed by city ID.
+    """
+    cities = ti.xcom_pull(task_ids='get_cities')
+    
+    d_cities = {}
+    for city in cities:
+        d_cities[city["id"]] = city
+    return d_cities
 
 with DAG(
     schedule_interval='*/15 * * * *', 
